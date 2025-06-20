14186423c1f421c676fadd3f3c91907d5b52345e
Barrack Makedi <barrackmakedi@gmail.com>
2025-06-20 20:06:48 +0300

    Updated inner project

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    # Bubble sort implementation
+    for i in range(len(unsorted_list) - 1, 0, -1):
+        for j in range(i):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                # Swap elements
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+
+    print("Bubble sort is done")
+unsorted_list= [5, 3, 8, 6, 7, 2]
+bubble_sort(unsorted_list)
+print(unsorted_list)  
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,17 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    
+    for i in range(n):
+        minpos = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[minpos]:
+                minpos = j
+        
+        # Swap only if needed
+        if minpos != i:
+            arr[i], arr[minpos] = arr[minpos], arr[i]
+    
+    print("Selection sort is done")
+arr = [6, 1, 0, 8, 7, 4]
+selection_sort(arr)
+print(arr) 
